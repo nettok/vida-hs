a87b4f39fd013ec95ecc280544697978eca22df6
Ernesto Menéndez <pyalec@gmail.com>
2018-05-09 21:16:57 -0600

    fix rendering positions

--- src/Main.hs
+++ src/Main.hs
@@ -10,21 +10,41 @@ type World = Life.UGrid IO Bool
 
 main :: IO ()
 main = do
-  let window = InWindow "Vida" (800, 800) (100, 50)
+  let window = InWindow "Vida" (width, height) (100, 50)
   let background = white
-  let fps = 1
-  initialState <- Life.makeEmptySquareUGrid 20
-  mapM_ (Life.runInstructionUGrid initialState) (Life.createGliderInstructions 10 10)
+  let fps = 5
+  initialState <- Life.makeEmptySquareUGrid 80
+  mapM_ (Life.runInstructionUGrid initialState) (Life.createGliderInstructions 1 1)
   playIO window background fps initialState render handleEvents update
 
+width = 800 :: Int
+height = 800 :: Int
+
 render :: World -> IO Picture
-render w = Pictures <$> Life.ifoldUGrid renderCell [] w
+render w = do
+  grid <- renderGrid w
+  Pictures . (:) grid <$> Life.ifoldUGrid renderCell [] w
   where
     renderCell :: Int -> Int -> Life.NeighbourRows IO Bool -> [Picture] -> Bool -> IO [Picture]
     renderCell x y _ acc c = return $
       if c
-        then Translate (fromIntegral x * 10) (fromIntegral y * 10) (Color black (circleSolid 5)) : acc
+        then newAliveCell x y : acc
         else acc
+
+    newAliveCell :: Int -> Int -> Picture
+    newAliveCell x y =
+      Translate posX posY (Color black (circleSolid cellRadius))
+      where
+        cellRadius = 5 :: Float
+        halfWidth = (fromIntegral width :: Float) / 2
+        halfHeight = (fromIntegral height :: Float) / 2
+        posX = fromIntegral (x * 10) - halfWidth + cellRadius
+        posY = fromIntegral (y * (-10)) + halfHeight - cellRadius
+
+    renderGrid :: World -> IO Picture
+    renderGrid w =
+      -- TODO: paint the real grid
+      return $ Pictures [Color red $ circleSolid 15, Color green $ Circle 20]
 
 handleEvents :: Event -> World -> IO World
 handleEvents _ = return
